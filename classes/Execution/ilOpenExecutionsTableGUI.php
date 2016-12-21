4f6e3252930f7f5a1f3a28998520419e0ae713eb
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2016-12-21 12:54:07 +0100

    fill row and inactivate some order options

--- classes/Execution/ilOpenExecutionsTableGUI.php
+++ classes/Execution/ilOpenExecutionsTableGUI.php
@@ -30,16 +30,22 @@ class ilOpenExecutionsTableGUI extends \ilTable2GUI
 		$this->configurateTable();
 	}
 
-	public function fillRow($row)
+	public function fillRow($execution)
 	{
-		$this->tpl->setVariable("SCHEDULED", $row["scheduled"]);
-		$this->tpl->setVariable("INDUCEMENT", $row["inducement"]);
-		$this->tpl->setVariable("LOGIN", $row["login"]);
-		$this->tpl->setVariable("FIRSTNAME", $row["firstname"]);
-		$this->tpl->setVariable("LASTNAME", $row["lastname"]);
-		$this->tpl->setVariable("ROLES", $row["roles"]);
-		$this->tpl->setVariable("INITIATOR", $row["initiator"]);
-		$this->tpl->setVariable("ACTIONS", $this->parent_object->getActionMenu($row["id"]));
+		$initiator = $execution->getInitator();
+		$users = $execution->getAction()->getUserCollection()->getUsers();
+		$roles = $execution->getAction()->getRoles();
+		$role_names = $this->getNameForRoles($roles);
+		$user = new \ilObjUser($users[0]);
+
+		$this->tpl->setVariable("SCHEDULED", $execution->getScheduled()->get(IL_CAL_FKT_DATE, "d.m.Y H:i:s"));
+		$this->tpl->setVariable("INDUCEMENT", $execution->getInducement());
+		$this->tpl->setVariable("LOGIN", $user->getLogin());
+		$this->tpl->setVariable("FIRSTNAME", $user->getFirstname());
+		$this->tpl->setVariable("LASTNAME", $user->getLastname());
+		$this->tpl->setVariable("ROLES", implode(", ", $role_names));
+		$this->tpl->setVariable("INITIATOR", $initiator->getLogin());
+		$this->tpl->setVariable("ACTIONS", $this->parent_object->getActionMenu($execution->getId()));
 	}
 
 	/**
@@ -53,19 +59,29 @@ class ilOpenExecutionsTableGUI extends \ilTable2GUI
 		$this->setTitle($this->txt("open_executions"));
 		$this->setTopCommands(false);
 		$this->setEnableHeader(true);
-		$this->setExternalSorting(false);
+		$this->setExternalSorting(true);
 		$this->setExternalSegmentation(false);
 		$this->setRowTemplate("tpl.open_executions_table_row.html", $this->plugin_object->getDirectory());
 		$this->setShowRowsSelector(false);
 
 		$this->addColumn($this->txt("scheduled"), "scheduled");
 		$this->addColumn($this->txt("inducement"), "inducement");
-		$this->addColumn($this->txt("login"), "login");
-		$this->addColumn($this->txt("firstname"), "firstname");
-		$this->addColumn($this->txt("lastname"), "lastname");
+		$this->addColumn($this->txt("login"));
+		$this->addColumn($this->txt("firstname"));
+		$this->addColumn($this->txt("lastname"));
 		$this->addColumn($this->txt("roles"));
-		$this->addColumn($this->txt("initiator"), "initiator");
+		$this->addColumn($this->txt("initiator"));
 		$this->addColumn($this->txt("actions"));
+	}
+
+	protected function getNameForRoles(array $roles)
+	{
+		$ret = array();
+		foreach ($roles as $role_id) {
+			$ret[] = \ilObject::_lookupTitle($role_id);
+		}
+
+		return $ret;
 	}
 
 	/**

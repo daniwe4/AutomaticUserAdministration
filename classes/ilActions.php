b283054362e339a461964cd4510038f1059403f8
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2016-12-21 11:06:54 +0100

    add create execution

--- classes/ilActions.php
+++ classes/ilActions.php
@@ -8,7 +8,7 @@ namespace CaT\Plugins\AutomaticUserAdministration;
  */
 class ilActions
 {
-	const F_ACTION = "action";
+	const F_INDUCEMENT = "inducement";
 	const F_LOGIN = "login";
 	const F_ROLES = "roles";
 	const F_SCHEDULED = "scheduled";
@@ -23,7 +23,31 @@ class ilActions
 		$this->execution_db = $execution_db;
 	}
 
-	public function getOpenActions()
+	/**
+	 * Create a new execution
+	 *
+	 * @param int 			$initiator_id
+	 * @param string 		$inducement
+	 * @param \ilDateTime 	$scheduled
+	 * @param \CaT\Plugin\AutomaticUserAdministration\Actions\UserAction 	$action
+	 * @param \ilDateTime 	$run_date
+	 */
+	public function createExeution(
+		$initiator_id,
+		$inducement,
+		\ilDateTime $scheduled,
+		\CaT\Plugin\AutomaticUserAdministration\Actions\UserAction $action,
+		\ilDateTime $run_date
+	) {
+		$this->execution_db->create($initiator_id, $inducement, $scheduled, $action, $run_date);
+	}
+
+	/**
+	 * Get all openen executens
+	 *
+	 * @return AutomaticUserAdministration\Execution\Execution[]
+	 */
+	public function getOpenExecutions()
 	{
 		$open_actions = $this->execution_db->getOpenExecutions();
 

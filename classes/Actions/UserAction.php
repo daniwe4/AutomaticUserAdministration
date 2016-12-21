2273dbd27abc0c58d9e803b1ea063c7621de729e
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2016-12-21 10:03:38 +0100

    new implementation

--- classes/Actions/UserAction.php
+++ classes/Actions/UserAction.php
@@ -2,38 +2,15 @@
 
 namespace CaT\Plugins\AutomaticUserAdministration\Actions;
 
-/**
- * Interface for user actions
- */
-interface UserAction extends \Serializable
+abstract class UserAction implements Action
 {
 	/**
-	 * get the name of action
-	 *
-	 * @return string
+	 * @var \CaT\Plugins\AutomaticUserAdministration\UserCollection
 	 */
-	public function getId();
+	protected $user_collection;
 
-	/**
-	 * Execute the action
-	 *
-	 * @return int[]
-	 */
-	public function run();
-
-	/**
-	 * Seralize the collection object values
-	 *
-	 * @return string
-	 */
-	public function serialize();
-
-	/**
-	 * Deserialize the collection object values
-	 *
-	 * @param string 	$data
-	 *
-	 * @return null
-	 */
-	public function deserialize($data);
+	public function __construct(\CaT\Plugins\AutomaticUserAdministration\Collections\UserCollection $user_collection)
+	{
+		$this->user_collection = $user_collection;
+	}
 }

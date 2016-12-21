06eb6cc7a1aed76f27fd854efd8d2f82c86a6ee5
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2016-12-21 10:07:12 +0100

    correct function calls

--- tests/BogusTest.php
+++ tests/BogusTest.php
@@ -3,24 +3,28 @@
 /**
  * Sample for PHP Unit tests
  */
-class BogusTest extends PHPUnit_Framework_TestCase {
+class BogusTest extends PHPUnit_Framework_TestCase
+{
 
-	public function test_successfull() {
+	public function test_successfull()
+	{
 		$test_var = "Peter";
 
-		$this->assertEqual("Peter", $test_var);
+		$this->assertEquals("Peter", $test_var);
 	}
 
-	public function test_failed() {
+	public function test_failed()
+	{
 		try {
 			$this->checkValue("Bernd");
 			$this->assertFalse("Should have raised.");
+		} catch (Exception $e) {
 		}
-		catch (Exception $e) {}
 	}
 
-	protected function checkValue($value) {
-		if($value != "Peter") {
+	protected function checkValue($value)
+	{
+		if ($value != "Peter") {
 			throw new Exception("Value is wrong");
 		}
 	}

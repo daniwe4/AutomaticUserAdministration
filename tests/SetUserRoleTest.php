5de91034eba0d4b0d5e4a5559d1e4bc9998c3ded
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2016-12-21 10:07:50 +0100

    add tests for single user role action

---
+++ tests/SetUserRoleTest.php
@@ -0,0 +1,42 @@
+<?php
+
+use \CaT\Plugins\AutomaticUserAdministration\Actions\SetUserRoles;
+use \CaT\Plugins\AutomaticUserAdministration\Collections\SingleUserCollection;
+use PHPUnit\Framework\TestCase;
+
+class SetUserRoleTest extends TestCase
+{
+
+	public function setUp()
+	{
+		$this->user_collection = $this->createMock("\\CaT\\Plugins\\AutomaticUserAdministration\\Collections\\SingleUserCollection");
+
+		$this->user_collection->method("serialize")
+							  ->willReturn(serialize(10));
+
+		$this->user_collection->method("unserialize");
+
+		$this->roles = array(2,3,4,5);
+	}
+
+	public function test_serialize()
+	{
+		$action = new SetUserRoles($this->user_collection, $this->roles);
+		$serialized_action = serialize($action);
+
+		$this->assertInternalType('string', $serialized_action);
+
+		return array($serialized_action, $action);
+	}
+
+	/**
+	 * @depends test_serialize
+	 */
+	public function test_unseralize($args)
+	{
+		$action = $args[1];
+		$unserialized_action = unserialize($args[0]);
+
+		$this->assertEquals($action, $unserialized_action);
+	}
+}
